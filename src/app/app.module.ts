77909aa6bfcb70cfc549cfe6e121625f147a367b
unknown <santiago.rg@live.com>
2022-11-15 18:42:17 -0600

    Version Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { NgxGraphModule } from '@swimlane/ngx-graph';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { DagreClusterLayout } from '@swimlane/ngx-graph/public_api';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,BrowserAnimationsModule, IonicModule.forRoot(), AppRoutingModule, NgxGraphModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
